867bd5ca30fc9489a41409052020d305c4782aeb
Octavian Geagla <ogeagla@gmail.com>
2014-06-01 18:37:50 -0600

    Use temporary staging directory to rename image files with spaces in them

--- generate-watermarked-and-thumbs.sh
+++ generate-watermarked-and-thumbs.sh
@@ -9,12 +9,24 @@ exec > >(tee logfile.txt)
 # I did not want to steal from him by simply adding his answer to mine.
 exec 2>&1
 
+WORKING=./img/working
 RAW_FOLDER=./img/raw/all
 WATERMARKS_FOLDER=./img/watermarks
 WATERMARKED_FOLDER=./img/watermarked
 THUMBS_FOLDER=./img/thumbnail
 
-for file in $RAW_FOLDER/*
+if [ ! -d "$WORKING" ]; then
+    mkdir "$WORKING"
+    cp -r $RAW_FOLDER/* $WORKING 
+    
+    #rename " " "_" "${WORKING}/*"
+    #cd path/to/dir
+    for file in $WORKING/*; do
+        mv --backup=numbered -- "$file" "${file// /_}"
+    done
+fi
+
+for file in $WORKING/*
 do
     
     # next line checks the mime-type of the file
@@ -30,17 +42,21 @@ do
         filename="${filename%.*}"
         
         WATERMARK_IMG="${WATERMARKS_FOLDER}/${filename}-watermark.png"
-        if [[ ! -f $WATERMARK_IMG ]]; then
+        if [[ ! -f "$WATERMARK_IMG" ]]; then
             echo "generating watermark for $file"
             echo "  image type: $IMAGE_TYPE"
             echo "  size: $IMAGE_SIZE, width: $WIDTH, height: $HEIGHT"
             #create trans img w watermark with custom size
-            S_W=4
-            S_H=20
-            W=$((WIDTH - $((WIDTH/S_W))))
-            H=$((HEIGHT - $((HEIGHT/S_H))))
+            #S_W=4
+            #S_H=20
+            #W=$((WIDTH - $((WIDTH/S_W))))
+            #H=$((HEIGHT - $((HEIGHT/S_H))))
+            D_W=120
+            D_H=30
+            W=$((WIDTH - D_W))
+            H=$((HEIGHT - D_H))
             echo "  mark location: $W, $H"
-            convert -size "$WIDTH"x"$HEIGHT" xc:transparent -font Carlito-bold -pointsize 32 -fill DeepSkyBlue -draw "text 20,30 'octavian g'" "$WATERMARK_IMG"
+            convert -size "$WIDTH"x"$HEIGHT" xc:transparent -font Carlito-bold -pointsize 32 -fill DeepSkyBlue -draw "text $W,$H 'octavian g'" "$WATERMARK_IMG"
             
             echo "  watermark: $WATERMARK_IMG"
             
@@ -66,7 +82,7 @@ do
         #`file -b $file | sed 's/ //g' | sed 's/,/ /g' | awk  '{print $2}'`
         WIDTH=`echo $IMAGE_SIZE | sed 's/x/ /g' | awk '{print $1}'`
         HEIGHT=`echo $IMAGE_SIZE | sed 's/x/ /g' | awk '{print $2}'`  
-                
+        
         # If the image width is greater that 200 or the height is greater that 150 a thumb is created
         #if [ $WIDTH -ge  201 ] || [ $HEIGHT -ge 151 ]; then
         #This line convert the image in a 200 x 150 thumb 
@@ -80,11 +96,11 @@ do
             echo "size: $IMAGE_SIZE, width: $WIDTH, height: $HEIGHT"
             #generate thumbnail:
             convert -sample 200x150 "$file" "$THUMB_IMG" 
- 
+            
         else
             echo "  file $THUMB_IMG already exists, skipping"
         fi
-       
+        
         echo "{
           \"url_wt\":\"${THUMBS_FOLDER}/${filename}_thumb.${extension}\",
           \"url_w\":\"${WATERMARKED_FOLDER}/${filename}.${extension}\",
